c11fb5dfc834ccbebff52be9b031f1d8af4c6ed3
Marine <marine.runavot@hotmail.fr>
2025-11-03 09:56:58 +0100

    Ajout composant React Data Table pour tableau de films

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
-import './App.css'
+import DataTable from "react-data-table-component";
 
 function App() {
-  const [recherche, setRecherche] = useState("")
-  const [films, setFilms] = useState([])
 
-  const url = "http://api.themoviedb.org/3/search/movie?api_key=f33cd318f5135dba306176c13104506a&query="
+  const columns = [
+      {
+        name: <b>Nom</b>,
+        selector: (film) => film.title,
+        sortable: true,
+      },
+      {
+        name: 'Année',
+        selector: (film) => film.release_date,
+        sortable: true,
+      },
+      {
+        name: 'Poster',
+        cell: (film) => <img style={{width: '60px'}} src={film.poster_path?'http://image.tmdb.org/t/p/w185' + film.poster_path:'https://placehold.co/185x274'} />,
+        
+      },
+      {
+        name: 'Vote',
+        selector: film => film.vote_average,
+        sortable: true
+      }
+    ];
+
+  const [films, setFilms] = useState([])
+  const [recherche, setRecherche] = useState("")
 
   const handleClick = () => {
-    axios.get(url+recherche)
-      .then(function(response) {
-        setFilms(response.data.results)
-      })
-      setRecherche("")
+    const adresse = "http://api.themoviedb.org/3/search/movie?api_key=f33cd318f5135dba306176c13104506a&query="
+
+    axios.get(adresse + recherche)
+    .then( (reponse) => {
+      setFilms(reponse.data.results)
+    })
+
+    setRecherche("")
   }
 
   const handleChange = (e) => {
@@ -22,23 +47,15 @@ function App() {
 
   return (
     <>
-      <input 
-        type="text" 
-        value={recherche} 
-        placeholder="Rechercher des films.."
-        onChange={handleChange}
-        >
-      </input>
+      <input type="text" placeholder="Saisir nom de films.." value={recherche} onChange={handleChange}></input>
       <button onClick={handleClick}>Rechercher</button>
-      <ul>
-        {
-          films.map( (film, id) => (
-            <li key={film.id}>
-              {film.title}
-            </li>
-          ))
-        }
-      </ul>
+      <h1>Liste des films</h1>
+
+      <DataTable
+          columns={columns}
+          data={films}
+          key={films.id}
+      />
     </>
   )
 }
